2ee7f25a26fa811217fefd7de1e3a9c8a7c2ed50
Daniel Rojas <daniel_rojas@hakkoda.io>
2025-06-05 19:53:59 +0000

    including transformations

--- models/landing/customer_lnd.sql
+++ models/landing/customer_lnd.sql
@@ -23,7 +23,7 @@ with source as (
         WHEN Marital_Status = 'Together' then 'Common-Law Union'
         WHEN Marital_Status in ('YOLO','Alone','Absurd') then 'Single'
         WHEN Marital_Status = 'Widow' then 'Widowed'
-        ELSE  Marital_Status END,
+        ELSE  Marital_Status END AS Marital_Status,
     COALESCE(Income,0):: number(38,2) AS INCOME,
     Kidhome,
     Teenhome,
@@ -34,11 +34,11 @@ with source as (
     MntFishProducts,
     MntSweetProducts,
     MntGoldProds,
-    NumDealsPurchases,
-    NumWebPurchases,
-    NumCatalogPurchases,
-    NumStorePurchases,
-    NumWebVisitsMonth,
+    NumDealsPurchases :: number(38,2), {# change the precision number during demo #}
+    NumWebPurchases :: number(38,2), 
+    NumCatalogPurchases :: number(38,2),  --example if we include sql comments in the model
+    NumStorePurchases :: number(38,2),
+    NumWebVisitsMonth :: number(38,2),
     AcceptedCmp3,
     AcceptedCmp4,
     AcceptedCmp5,
@@ -46,8 +46,8 @@ with source as (
     AcceptedCmp2,
     Complain,
     Z_CostContact,
-    Z_Revenue,
-    Response,
+    Z_Revenue:: number(38,2),
+    Response :: boolean ,
     TO_DATE(Dt_Customer,'DD-MM-YYYY') AS DATE
 
     from 
